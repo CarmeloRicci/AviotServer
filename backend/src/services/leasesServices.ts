70e90eb1260200930c9192ed71c219bf69efa632
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-20 12:41:14 +0200

    ok

--- backend/src/services/leasesServices.ts
+++ backend/src/services/leasesServices.ts
@@ -15,31 +15,13 @@ export default class LeasesServices {
         let data = fs.readFileSync('/var/lib/misc/dnsmasq.leases', 'utf8');
         //console.log('Leases', data);
         let splitted1 = data.split("\n");
-        console.log ("1-->" + splitted1[0])
-        console.log ("0-->" + splitted1[1])
         for (let i in splitted1) {
-            //console.log(splitted1)
-            //console.log(i)
             let splitted2 = splitted1[i].split(" ");
             if (splitted2.length === 5) {
-                //for (let i in splitted2) {
-                    temp = { timestamp: splitted2[0], mac: splitted2[1], ip: splitted2[2], host: splitted2[3], id: splitted2[4] };
-                //}
+                temp = { timestamp: splitted2[0], mac: splitted2[1], ip: splitted2[2], host: splitted2[3], id: splitted2[4] };
                 leases_file.push(temp)
             }
-            // let splitted2 = splitted1[1].split(" ");
-            // if (splitted2.length === 5) {
-            //     for (let i in splitted2) {
-            //     temp.timestamp = splitted2[0]
-            //     temp.mac = splitted2[1]
-            //     temp.ip = splitted2[2]
-            //     temp.host = splitted2[3]
-            //     temp.id = splitted2[4]
-            //     }
-            // }
-            // leases_file.push(temp)
         }
         console.log(leases_file)
     }
-
 }
