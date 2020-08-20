8b87db2e6d205e42745f756e9cfea2ec4a09cc38
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-20 11:27:13 +0200

    ok

--- backend/src/services/leasesServices.ts
+++ backend/src/services/leasesServices.ts
@@ -4,11 +4,13 @@ const equal = require('deep-equal');
 const fs = require('fs');
 import { Utilities } from '../shared/utilities';
 import _ = require('lodash');
+const leases = require('dnsmasq-leases');
 
 
 export default class LeasesServices {
     async leasesServices(ip: string) {
-
+        let data = fs.readFileSync('/var/lib/misc/dnsmasq.leases', 'utf8');
+        console.log('Leases',leases(data));
     }
 
 }
